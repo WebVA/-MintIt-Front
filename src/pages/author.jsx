767171e076e4f642bd1a5ea5e11470d7f8e618dd
mastercodercat <mastercoderoy64@gmail.com>
2022-09-14 12:11:22 -0400

    Updated author/myprofile pages

--- src/pages/author.jsx
+++ src/pages/author.jsx
@@ -2,8 +2,8 @@ import SEO from "@components/seo";
 import Wrapper from "@layout/wrapper";
 import Header from "@layout/header/header-01";
 import Footer from "@layout/footer/footer-01";
-import AuthorIntroArea from "@containers/author-intro";
-import AuthorProfileArea from "@containers/author-profile";
+import AuthorIntroArea from "@containers/author-intro/layout-02";
+import AuthorProfileArea from "@containers/author-profile/layout-02";
 
 // Demo data
 import authorData from "../data/author.json";
--- src/pages/mycollection.jsx
+++ src/pages/mycollection.jsx
@@ -2,8 +2,8 @@ import SEO from "@components/seo";
 import Wrapper from "@layout/wrapper";
 import Header from "@layout/header/header-01";
 import Footer from "@layout/footer/footer-01";
-import AuthorIntroArea from "@containers/author-intro";
-import AuthorProfileArea from "@containers/author-profile";
+import AuthorIntroArea from "@containers/author-intro/layout-01";
+import AuthorProfileArea from "@containers/author-profile/layout-01";
 
 // Demo data
 import authorData from "../data/author.json";
@@ -15,7 +15,7 @@ export async function getStaticProps() {
 
 const Author = () => (
     <Wrapper>
-        <SEO pageTitle="Author" />
+        <SEO pageTitle="MyCollection" />
         <Header />
         <main id="main-content">
             <AuthorIntroArea data={authorData} />
