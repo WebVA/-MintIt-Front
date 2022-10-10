38550e01a2796948ad3879202c12c8c947c45d90
Jacob Yi <jacobyidev@gmail.com>
2022-10-10 11:50:03 -0700

    fix: Update proof for starting index on provenance page

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,3 +2,12 @@ export const EXAMPLE_PATH = "blog-starter";
 export const CMS_NAME = "Markdown";
 export const HOME_OG_IMAGE_URL =
     "https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg";
+export const PROOF_FOR_STARTING_INDEX = `(defun random-integer:integer
+    ()
+    (bind (chain-data)
+        { 
+            'block-height := block-height, 
+            'block-time := block-time
+        }
+    (round (* block-height (diff-time block-time (time "1970-01-01T00:00:00Z")))))
+)`;
