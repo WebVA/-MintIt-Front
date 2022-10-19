f5f546ac23d0faae1576c22cc98175e3ad9423b5
Ag <aravindmallapureddy@juspay.in>
2022-10-19 11:29:14 +0530

    Make the slug a lower case word.

--- src/containers/author-profile/layout-02/index.jsx
+++ src/containers/author-profile/layout-02/index.jsx
@@ -165,9 +165,9 @@ const DublicateCollectionArea = ({ className, data }) => {
                                             overlay
                                             placeBid
                                             title={prod["collection-name"]}
-                                            slug={prod[
-                                                "collection-name"
-                                            ].replace(/ /g, "-")}
+                                            slug={prod["collection-name"]
+                                                .replace(/ /g, "-")
+                                                .toLowerCase()}
                                             hash={prod["content-hash"]}
                                             latestBid={prod.latestBid}
                                             //dummy data
